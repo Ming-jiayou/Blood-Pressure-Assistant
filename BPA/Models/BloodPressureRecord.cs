6bd72d39cd9ae795dded48b5b6303d0ab76612ad
mingupup <2539823901@qq.com>
2025-06-30 10:46:38 +0800

    实现了记录血压的基本功能

---
+++ BPA/Models/BloodPressureRecord.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BPA.Models
+{
+    public class BloodPressureRecord
+    {
+        public int Systolic { get; set; }  // 收缩压（高压）
+        public int Diastolic { get; set; }  // 舒张压（低压）
+        public int HeartRate { get; set; }  // 心率
+        public DateTime Timestamp { get; set; } = DateTime.Now;
+    }
+} 
--- BPA/ViewModels/Pages/BPAHomeViewModel.cs
+++ BPA/ViewModels/Pages/BPAHomeViewModel.cs
@@ -3,12 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using BPA.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace BPA.ViewModels.Pages
 {
     public partial class BPAHomeViewModel : ObservableObject
     {
         [ObservableProperty]
+        private int systolic;
+
+        [ObservableProperty]
+        private int diastolic;
+
+        [ObservableProperty]
+        private int heartRate;
+
+        [ObservableProperty]
+        private ObservableCollection<BloodPressureRecord> records = new();
+
+        [ObservableProperty]
         string home = "你好啊";
+
+        [RelayCommand]
+        private void SaveRecord()
+        {
+            var record = new BloodPressureRecord
+            {
+                Systolic = Systolic,
+                Diastolic = Diastolic,
+                HeartRate = HeartRate,
+                Timestamp = DateTime.Now
+            };
+
+            Records.Insert(0, record);
+            
+            // Reset input fields
+            Systolic = 0;
+            Diastolic = 0;
+            HeartRate = 0;
+        }
     }
 }
